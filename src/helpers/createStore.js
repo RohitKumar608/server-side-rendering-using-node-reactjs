644b741922ecec3210d2d0c3c83ebca6f3655810
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-11-15 15:30:45 +0530

    feat: initialsed state from data coming from server side in window.INTIAL_STATE

--- src/helpers/createStore.js
+++ src/helpers/createStore.js
@@ -5,17 +5,20 @@ import reducers from '../client/reducers'
 import webConfig from '../../webConfig.json'
 
 export default (req) => {
-
+    let initialState = {}
+    if (typeof window !== 'undefined') {
+        initialState = window?.INITIAL_STATE
+    }
     const axiosInstance = axios.create({
         baseURL: webConfig.axiosInstance_baseURL,
         headers: {
             cookie: req.get('cookie') || ''
         }
     })
-
+    
     const store = createStore(
-        reducers, 
-        {}, 
+        reducers,
+        initialState, 
         applyMiddleware(thunk.withExtraArgument(axiosInstance)
     ))
     return store
